4f5573f13ba247b6a4e5b2fcf0426281ef7360d8
blakecondrey <condrey.blake1217@gmail.com>
2022-01-31 11:09:50 -0700

    auth controller error handling updated with custom errors created, badrequest, notfounderror, all from customapierror. error-handler.js updated for precise error message delivery in api instead of error object by mapping through body and outputting specific message.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,18 @@
 import User from "../models/User.js";
 import { StatusCodes } from "http-status-codes";
 
+import { BadRequestError } from "../errors/index.js";
+
 const register = async (req, res, next) => {
   // requires registrant to provide all values -> await User.create(req.body)
-  const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("Please provide all values.");
+  }
+
+  const user = await User.create({ name, email, password });
+  res.status(StatusCodes.OK).json({ user });
 };
 
 const login = async (req, res) => {
